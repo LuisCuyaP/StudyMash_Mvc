6f820006f1d77a884b096033c10e9a5f63edfeff
Luis <luiscuya@gmail.com>
2023-03-18 15:49:01 -0500

    1 commit - se agrega el dbcontext y los modelos iniciales

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyMash_MVC.Models;
+
+namespace StudyMash_MVC.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) { }
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+    }
+}
